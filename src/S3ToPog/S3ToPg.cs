6f401aed5f56a779a18b56188f02751a06551224
Kyle Gobel <kgobel@gmail.com>
2015-12-12 10:29:22 -0500

    add manifest support, and depend on Jil

--- src/S3ToPog/S3ToPg.cs
+++ src/S3ToPog/S3ToPg.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Data;
 using System.IO;
+using System.IO.Compression;
 using System.Text.RegularExpressions;
 using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
+using Jil;
 using Npgsql;
 
 namespace S3ToPg
 {
+    public class S3Manifest
+    {
+        public S3Entry[] Entries { get; set; }
+    }
+
+    public class S3Entry
+    {
+        public string Url { get; set; }
+    }
     public class S3ToPgConfig
     {
         public AWSCredentials Credentials { get; set; }
@@ -32,10 +43,43 @@ namespace S3ToPg
 
     public static class S3LoaderExtensions
     {
-        public static void LoadFromS3(this NpgsqlConnection connection, string s3Path, string tableName)
+        public static void LoadFromS3(this NpgsqlConnection connection, string s3Path, string tableName, bool gzipped = false, bool manifest = false)
         {
             var s3Loader = new S3Loader();
-            s3Loader.Load(s3Path, tableName, connection);
+            var tuple = Utils.ParseS3Path(s3Path);
+            if (tuple == null)
+            {
+                throw new ArgumentException("Couldn't parse s3Path");
+            }
+
+            if (manifest)
+            {
+                var manifestFile = s3Loader.LoadManifest(tuple.Item1, tuple.Item2);
+                foreach (var entry in manifestFile.Entries)
+                {
+                    var tup = Utils.ParseS3Path(entry.Url);
+                    if (gzipped)
+                    {
+                        s3Loader.LoadGzipped(tup.Item1, tup.Item2, S3ToPgConfig.Instance.Delimiter, tableName,
+                            connection);
+                    }
+                    else
+                    {
+                        s3Loader.Load(tup.Item1, tup.Item2, tableName, S3ToPgConfig.Instance.Delimiter, connection);
+                    }
+                }
+            }
+            else
+            {
+                if (gzipped)
+                {
+                    s3Loader.LoadGzipped(tuple.Item1, tuple.Item2, S3ToPgConfig.Instance.Delimiter, tableName, connection);
+                }
+                else
+                {
+                    s3Loader.Load(tuple.Item1, tuple.Item2, tableName, S3ToPgConfig.Instance.Delimiter, connection);
+                }
+            }
         }
     }
     public class S3Loader
@@ -55,10 +99,6 @@ namespace S3ToPg
             _s3Client = new AmazonS3Client(_config.Credentials, _config.Region);
         }
 
-        public void Load(string bucket, string key, string tableName, NpgsqlConnection connection)
-        {
-            Load(bucket, key, tableName, _config.Delimiter, connection);
-        }
         public void Load(string bucket, string key, string tableName, string delimiter, NpgsqlConnection connection)
         {
             var needsOpening = connection.State != ConnectionState.Open;
@@ -82,17 +122,44 @@ namespace S3ToPg
             }
         }
 
-        public void Load(string s3Path, string tableName, NpgsqlConnection connection)
+        public S3Manifest LoadManifest(string bucket, string key)
         {
-            var tuple = ParseS3Path(s3Path);
-            if (tuple == null)
+            var s3Obj = _s3Client.GetObject(bucket, key);
+
+            using (var reader = new StreamReader(s3Obj.ResponseStream))
             {
-                throw new ArgumentException($"Couldn't parse s3 path '${s3Path}'");
+                var manifest = reader.ReadToEnd();
+                return JSON.Deserialize<S3Manifest>(manifest, Options.CamelCase);
             }
-            Load(tuple.Item1, tuple.Item2, tableName, _config.Delimiter, connection);
         }
 
-        private Tuple<string, string> ParseS3Path(string path)
+        public void LoadGzipped(string bucket, string key, string delimiter, string tableName, NpgsqlConnection connection)
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            var s3Obj = _s3Client.GetObject(bucket, key);
+            using (var decompressStream = new GZipStream(s3Obj.ResponseStream, CompressionMode.Decompress))
+            using (var reader = new StreamReader(decompressStream))
+            {
+                var copyCommand = string.Format("COPY {0} FROM STDIN (DELIMITER '{1}')", tableName, delimiter);
+                using (var writer = connection.BeginTextImport(copyCommand))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Utils
+    {
+        public static Tuple<string, string> ParseS3Path(string path)
         {
             const string pattern = @"s3://(?<BucketName>[^/]*)/(?<FolderName>.*)$";
             var match = Regex.Match(path, pattern);
